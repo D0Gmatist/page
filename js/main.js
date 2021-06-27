dfee25e8dfb5d596de61cf04cd8da1a55f754c5d
Сергей Тарасевич <s.tarasevich@e-vision.by>
2021-06-27 16:29:54 +0300

    add finish

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
 let pageClass = {
-	
+	doc: $( document ),
+	win: $( window ),
 	slick: {
 		init: () => {
+
 			$('.multiple-items').slick({
 				infinite: true,
 				slidesToShow: 3,
 				slidesToScroll: 3,
 				autoplay: false,
 				autoplaySpeed: 4000,
-				prevArrow: '<button class="slick-prev page-slide-prev" aria-label="Previous" type="button">Previous</button>',
-				nextArrow: '<button class="slick-next page-slide-next" aria-label="Next" type="button">Next</button>',
+				prevArrow: '<button class="slick-prev page-slide-prev" aria-label="Previous" type="button"><i class="icon-prev"></i></button>',
+				nextArrow: '<button class="slick-next page-slide-next" aria-label="Next" type="button"><i class="icon-next"></i></button>',
 				responsive: [
 					{
-						breakpoint: 426,
+						breakpoint: 767,
 						settings: {
 							slidesToScroll: 1,
 							slidesToShow: 1
 						}
 					},
 					{
-						breakpoint: 1025,
+						breakpoint: 1279,
 						settings: {
 							slidesToScroll: 2,
 							slidesToShow: 2
@@ -28,17 +30,62 @@ let pageClass = {
 				]
 			});
 			
+		},
+		filter: () => {
+			let element = document.getElementsByClassName( 'filter-btn active' );
+			Array.from(element).forEach( ( el ) => {
+				if ( el.getAttribute( 'id' ) ) {
+					$('.multiple-items').slick('slickFilter','.' + el.getAttribute( 'id' ));
+					
+				}
+				
+			});
+			
+		},
+		unFilter: () => {
+			$('.multiple-items').slick('slickUnfilter');
+			
 		}
 
 	},
+	evens: {
+		add: () => {
+			let element = document.getElementsByClassName("filter-btn");
+			Array.from(element).forEach( ( el ) => {
+				el.addEventListener( 'click', event => {
+					pageClass.evens.element( event );
+					
+				});
+				
+			});
+			
+		},
+		element: ( event ) => {
+			if ( ! Array.from(event.target.classList).includes( 'active' ) ) {
+				let element = document.getElementsByClassName( 'filter-btn active' );
+				Array.from(element).forEach( ( el ) => {
+					pageClass.slick.unFilter();
+					el.classList.remove( 'active' );
+					
+				});
+				event.target.classList.add( 'active' );
+				pageClass.slick.filter();
+				
+				
+			}
+			
+		}
+	},
 	ready: () => {
+		pageClass.evens.add();
 		pageClass.slick.init();
+		pageClass.slick.filter();
 
 	}
 	
 }
 
-$( document ).ready(() => {
+pageClass.doc.ready(() => {
 	pageClass.ready();
 	
 });
